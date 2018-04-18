a28c9305aab5759e2b35f28eb892a6aa29726a6a
Jaroslav Vozár <jarkovozar@gmail.com>
2018-04-18 13:29:30 +0200

    Added script files (which should have been included in previous commits)

---
+++ Ludum Dare 41/Assets/Scripts/UIManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour {
+
+    public Toggle fullScreenToggleButton;
+    public Dropdown graphicsQualityDropdownButton;
+
+    private void Start()
+    {
+        fullScreenToggleButton.isOn = Screen.fullScreen;
+
+        graphicsQualityDropdownButton.value = QualitySettings.GetQualityLevel();
+    }
+
+    public void ToggleFullScreen(bool newState)
+    {
+        Screen.fullScreen = newState;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GraphicsQualityChanged(int newQquality)
+    {
+        QualitySettings.SetQualityLevel(newQquality, true);
+    }
+
+    public void ChangeVolume(float newValue)
+    {
+        AudioListener.volume = newValue;
+    }
+}
