8ee02cffd1b8dae1c06f486ae60d74c5c2244054
ParabyteX <Bizbud@live.com>
2018-04-21 17:37:22 -0400

    Song Manager Edits

--- Ludum Dare 41/Assets/Scripts/UserSongManager.cs
+++ Ludum Dare 41/Assets/Scripts/UserSongManager.cs
@@ -5,6 +5,8 @@ using System.IO;
 
 public class UserSongManager : MonoBehaviour 
 {
+	[SerializeField] AudioSource source;
+
 	void Start()
 	{
 		StartCoroutine(loadAudioClip());
@@ -12,20 +14,35 @@ public class UserSongManager : MonoBehaviour
 	private IEnumerator loadAudioClip()
 	{
 		string path = "../UserSongs/";
-		string fileExtension = "*.wav";
+		string fileExtension = "*.ogg";
 		string[] filePaths = Directory.GetFiles(path, fileExtension);
 		Debug.Log(filePaths[0]);
 
 		List <AudioClip> audioClips = new List<AudioClip>();
 		for(int i = 0; i < filePaths.Length; i++)
 		{
-			WWW audioClipDir = new WWW("file://" + filePaths[i]);
+			WWW audioClipDir = new WWW("https://github.com/jerry411/LudumDare41/blob/master/UserSongs/CaveChiptune.wav"); //+ filePaths[i]);
+			//WWW audioClipDir = new WWW("../UserSongs/" + filePaths[i]);
+			//WWW audioClipDir = new WWW(filePaths[i]);
 
 			while(!audioClipDir.isDone)
 			{
 				yield return null;
 			}
 			audioClips.Add(audioClipDir.GetAudioClip());
+			if(audioClips[i] == null)
+			{
+				Debug.Log("Fucking null");
+			}
+			SetAudioClip(audioClips[i]);
+		}
+	}
+	private void SetAudioClip(AudioClip clip)
+	{
+		source.clip = clip;
+		if(!source.isPlaying)
+		{
+			source.Play();
 		}
 	}
 }
