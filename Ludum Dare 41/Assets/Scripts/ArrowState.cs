74d928f403e3527d871d1b2d5174aad70aee3cec
Jaroslav Vozár <jarkovozar@gmail.com>
2018-04-21 21:57:11 +0200

    Added numerous scripts for arrows detection and control

---
+++ Ludum Dare 41/Assets/Scripts/ArrowState.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowState : MonoBehaviour
+{
+    Canvas hitArea;
+
+    AccelerationCorrectArrows stats;
+
+    public bool active;
+
+    public bool isInHitArea;
+
+    public bool isHit;
+
+	void Start ()
+	{
+        active = true;
+    }
+	
+	void Update ()
+	{
+	    float translation = Screen.width / 2f / 2f * Time.deltaTime;
+	    transform.Translate(new Vector3(-translation, 0f, 0f));
+
+	    if (transform.position.x < - 50)
+	    {
+	        Destroy(gameObject);
+	    }
+    }
+
+    void GotAfterHitArea()
+    {
+        if (!isHit)
+        {
+            stats.AddMalus();
+            stats.BreakStreak();
+        }
+
+        hitArea.GetComponent<HitArea>().arrowsInHitArea.Remove(gameObject);
+    }
+
+    void Hit()
+    {
+        isHit = true;
+
+        stats.AddToStreak();
+        stats.BreakMalus();
+
+        //TODO: change color/fade
+    }
+}
---
+++ Ludum Dare 41/Assets/Scripts/HitArea.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitArea : MonoBehaviour
+{
+    public List<GameObject> arrowsInHitArea;
+
+    private void Start()
+    {
+        arrowsInHitArea = new List<GameObject>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        arrowsInHitArea.Add(other.gameObject);
+        other.gameObject.GetComponent<ArrowState>().isInHitArea = true;
+    }
+}
