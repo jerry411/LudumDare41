1c88074a97c469a700fbeaa0ed4aa1d0cd9f1b21
Jaroslav Vozár <jarkovozar@gmail.com>
2018-04-21 14:47:26 +0200

    Advances in arrow generator Added several sound files Added test audio file Created prefabs for arrows

--- Ludum Dare 41/Assets/Scripts/ArrowGenerator.cs
+++ Ludum Dare 41/Assets/Scripts/ArrowGenerator.cs
@@ -6,46 +6,56 @@ public class ArrowGenerator : MonoBehaviour
 {
     public AudioProcessor processor;
 
+    public AudioSource mainSongSource;
 
+    public GameObject arrowUp;
+    public GameObject arrowDown;
+    public GameObject arrowLeft;
+    public GameObject arrowRight;
 
-    enum arrows
-    {
-        up,
-        down,
-        left,
-        right
-    };
+    GameObject[] arrows;
 
 
     void Start ()
 	{
 	    processor.onBeat.AddListener(onBeatDetected);
-	    processor.onSpectrum.AddListener(onSpectrum);
+	    //processor.onSpectrum.AddListener(onSpectrum);
+
+	    arrows = new GameObject[] { arrowUp, arrowDown, arrowLeft, arrowRight };
+
+	    mainSongSource.PlayDelayed(2);
 	}
 
     //This event will be called every time a beat is detected.
     //Change the threshold parameter in the inspector to adjust the sensitivity
     void onBeatDetected()
     {
-        //Debug.Log("Beat!!!");
+
+        Debug.Log(selectRandomArrow());
+
+        displayArrow(selectRandomArrow());
     }
 
     //This event will be called every frame while music is playing
-    void onSpectrum(float[] spectrum)
+    /*void onSpectrum(float[] spectrum)
     {
-
-
         for (int i = 0; i < spectrum.Length; ++i)
         {
             Debug.Log(spectrum[i]);
-            //Vector3 start = new Vector3(i, 0, 0);
-            //Vector3 end   = new Vector3(i, spectrum[i], 0);
-            //Debug.DrawLine(start, end);
+            Vector3 start = new Vector3(i, 0, 0);
+            Vector3 end   = new Vector3(i, spectrum[i], 0);
+            Debug.DrawLine(start, end);
         }
+    }*/
+
+    int selectRandomArrow()
+    {
+        return Random.Range(0, 4);
     }
 
-    void displayArrow()
+    void displayArrow( int index )
     {
+        GameObject selectedArrow = arrows[index];
 
     }
 }
