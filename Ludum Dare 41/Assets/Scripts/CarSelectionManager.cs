eca6bf9ab0ae0bef4f6e38b289589fe4e8667c29
ParabyteX <Bizbud@live.com>
2018-04-21 10:47:23 -0400

    Added stat dropdown in car selection

---
+++ Ludum Dare 41/Assets/Scripts/CarSelectionManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSelectionManager : MonoBehaviour 
+{
+	[SerializeField] private List <GameObject> StatDropdowns = new List <GameObject>();
+
+	public void DisplayCarStats(int StatDropdownIndex)
+	{
+		StatDropdowns[StatDropdownIndex].SetActive(true);
+	}
+	public void HideCarStats(int StatDropdownIndex)
+	{
+		StatDropdowns[StatDropdownIndex].SetActive(false);
+	}
+}
