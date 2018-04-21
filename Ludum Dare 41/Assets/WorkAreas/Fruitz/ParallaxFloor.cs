d51e3ba81f6a41a234425c134876f93e25ffa55d
Bhavya <bhavyamuni786@gmail.com>
2018-04-21 16:15:48 +0530

    .

--- Ludum Dare 41/Assets/WorkAreas/Fruitz/ParallaxFloor.cs
+++ Ludum Dare 41/Assets/WorkAreas/Fruitz/ParallaxFloor.cs
@@ -1,21 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ParallaxFloor : MonoBehaviour {
 
-    public Camera cam;
-    public Transform[] roads;
-    private float viewZone
-
-    private void Start()
-    {
-        cam = GameObject.FindGameObjectWithTag("Camera").GetComponent<Camera>();
-    }
-
-    // Update is called once per frame
-    void FixedUpdate ()
-    {
-		cam.vi
-	}
 }
