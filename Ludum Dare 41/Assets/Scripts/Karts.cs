a8c02aa70b9f64c5a7113941be6e861232424412
Bhavya <bhavyamuni786@gmail.com>
2018-04-21 21:01:06 +0530

    Merge branch 'master' of https://github.com/jerry411/LudumDare41

--- Ludum Dare 41/Assets/Scripts/Karts.cs
+++ Ludum Dare 41/Assets/Scripts/Karts.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class Karts {
-    float thrust = 15f;
+[CreateAssetMenu(fileName = "Kart", menuName = "Karts")]
+public class Karts : ScriptableObject {
+    public float thrust = 150f;
     Rigidbody2D rb;
 
-    public void MoveAhead()
-    {
-        rb.AddForce(rb.gameObject.transform.right * thrust);
-    }
-
-    public Karts(GameObject go)
+    public void MoveAhead(GameObject go)
     {
         rb = go.GetComponent<Rigidbody2D>();
+        rb.AddForce(rb.gameObject.transform.right * thrust);
     }
 }
