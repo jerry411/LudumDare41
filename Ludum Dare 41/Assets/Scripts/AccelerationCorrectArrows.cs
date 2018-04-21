eec798c87229c43f7eb86126b18a1d88e79caa0d
Bhavya <bhavyamuni786@gmail.com>
2018-04-21 23:24:24 +0530

    accele

---
+++ Ludum Dare 41/Assets/Scripts/AccelerationCorrectArrows.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerationCorrectArrows : MonoBehaviour {
+
+    private int streakCount = 0;
+    private int malusStreakCounter = 0;
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void AddToStreak()
+    {
+        streakCount += 1;
+    }
+
+    void BreakStreak()
+    {
+        streakCount = 0;
+        AddMalus();
+    }
+
+    void AddMalus()
+    {
+        malusStreakCounter += 1;
+    }
+
+    void BreakMalus()
+    {
+        malusStreakCounter = 0;
+        AddToStreak();
+    }
+
+
+}
