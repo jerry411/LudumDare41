b3c0189c55902f82f0c6c04e4f194ebaf98b454a
Bhavya <bhavyamuni786@gmail.com>
2018-04-22 19:45:33 +0530

    Pause menu

---
+++ Ludum Dare 41/Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+
+    public static bool gamePaused = false;
+    public GameObject pauseMenuCanvas;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+                Resume();
+            else
+                Pause();
+        }
+	}
+
+    public void Resume()
+    {
+        pauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0.0f;
+        gamePaused = true;
+    }
+}
