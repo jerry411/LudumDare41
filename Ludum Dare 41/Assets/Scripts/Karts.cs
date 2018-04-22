30c9521fae3486e0addf41bbf46530fe10c2d5bb
Bhavya <bhavyamuni786@gmail.com>
2018-04-22 14:53:54 +0530

    acceleration and deceleration of the car....

--- Ludum Dare 41/Assets/Scripts/Karts.cs
+++ Ludum Dare 41/Assets/Scripts/Karts.cs
@@ -5,7 +5,9 @@ using UnityEditor;
 
 [CreateAssetMenu(fileName = "Kart", menuName = "Karts")]
 public class Karts : ScriptableObject {
-    public float thrust = 150f;
+    public float thrust = 15f;
+    public float accelerationThrust = 5f;
+    public float malusThrust = -10f;
     Rigidbody2D rb;
 
     public void MoveAhead(GameObject go)
@@ -13,4 +15,33 @@ public class Karts : ScriptableObject {
         rb = go.GetComponent<Rigidbody2D>();
         rb.AddForce(rb.gameObject.transform.right * thrust);
     }
+
+    public void StreakBoost(GameObject go)
+    {
+        rb = go.GetComponent<Rigidbody2D>();
+        rb.drag -= 0.2f;
+        rb.AddForce(rb.gameObject.transform.right * accelerationThrust);
+    }
+
+    public void StreakSlowDown(GameObject go)
+    {
+        rb = go.GetComponent<Rigidbody2D>();
+        rb.drag += 0.2f;
+        rb.AddForce((-rb.gameObject.transform.right) * accelerationThrust);
+    }
+
+    public void MalusSlow(GameObject go)
+    {
+        rb = go.GetComponent<Rigidbody2D>();
+        rb.drag += 0.4f;
+        rb.AddForce(rb.gameObject.transform.right * malusThrust);
+    }
+
+    public void MalusFastAgain(GameObject go)
+    {
+        rb = go.GetComponent<Rigidbody2D>();
+        rb.drag -= 0.4f;
+        rb.AddForce(-rb.gameObject.transform.right * malusThrust);
+    }
+
 }
