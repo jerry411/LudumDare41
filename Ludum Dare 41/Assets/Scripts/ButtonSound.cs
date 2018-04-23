2551721c940610d4583535cffaf94f1b184454b0
Jaroslav Vozár <jarkovozar@gmail.com>
2018-04-23 02:59:03 +0200

    Added sounds for button clicks Small graphical fixes of buttons

---
+++ Ludum Dare 41/Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+    public AudioSource audio;
+    public bool playClickSoundAtInit = false;
+
+    private void Start()
+    {
+        if (playClickSoundAtInit)
+        {
+            audio.Play();
+        }
+    }
+
+    public void playSound()
+    {
+        audio.Play();
+    }
+}
