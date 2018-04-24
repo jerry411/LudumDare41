6cc68af44f08b98e576cc875ba7392de2913aee7
Jaroslav Vozár <jarkovozar@gmail.com>
2018-04-24 03:39:21 +0200

    Small changes

--- Ludum Dare 41/Assets/Scripts/Karts.cs
+++ Ludum Dare 41/Assets/Scripts/Karts.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 [CreateAssetMenu(fileName = "Kart", menuName = "Karts")]
 public class Karts : ScriptableObject {
